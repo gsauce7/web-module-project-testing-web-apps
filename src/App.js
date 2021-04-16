ad12f90c0c269bd5822cdd8226b32d62548eb2df
Gabriel Henton <gabehenton@gmail.com>
2021-04-15 18:10:02 -0700

    MVP complete React Testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import ContactForm from "./components/ContactForm";
+import ContactForm from './components/ContactForm';
 
 export default function App() {
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Integration Testing Challenge</a>
+      <nav className='navbar navbar-dark bg-dark'>
+        <a className='navbar-brand' href='#'>
+          <img width='40px' src='./Lambda-Logo-Red.png' alt='lambda logo' />{' '}
+          Lambda Integration Testing Challenge
+        </a>
       </nav>
-      <div className="App">
+      <div className='App'>
         <ContactForm />
       </div>
     </div>
